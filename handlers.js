60d2b47757dd47fb309ded351efcdc0b45b5e1a0
James Becwar <jbecwar@gmail.com>
2018-02-24 00:52:15 -0600

    Adds Hapi API

---
+++ handlers.js
@@ -0,0 +1,10 @@
+exports.setSpeed = async function setSpeed(request, h)
+{
+    throw new Error("Need to set speed here.");
+};
+
+exports.stop = async function stop(request,h)
+{
+    throw new Error("need to stop here");
+};
+
--- index.js
+++ index.js
@@ -1,80 +1,57 @@
-var rpio = require("rpio");
+'use strict';
 
+const Hapi = require('hapi');
+const handlers = require('./handlers');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+const Joi = require('joi');
 
-// Registers/etc:
-const PCA9685_ADDRESS = 0x40;
-const MODE1 = 0x00;
-const MODE2 = 0x01;
-const SUBADR1 = 0x02;
-const SUBADR2 = 0x03;
-const SUBADR3 = 0x04;
-const PRESCALE = 0xFE;
-const LED0_ON_L = 0x06;
-const LED0_ON_H = 0x07;
-const LED0_OFF_L = 0x08;
-const LED0_OFF_H = 0x09;
-const ALL_LED_ON_L = 0xFA;
-const ALL_LED_ON_H = 0xFB;
-const ALL_LED_OFF_L = 0xFC;
-const ALL_LED_OFF_H = 0xFD;
+const server = new Hapi.Server({ port: 3000, host: 'localhost' });
 
-//Bits:
-const RESTART = 0x80;
-const SLEEP = 0x10;
-const ALLCALL = 0x01;
-const INVRT = 0x10;
-const OUTDRV = 0x04;
-
-var write8 = function write8(register, data) {
-    rpio.i2cWrite(new Buffer([register, data]));
-}
-
-
-var init = function init() {
-    //Initialize the PCA9685.
-    //Setup I2C interface for the device.
-    set_all_pwm(0, 0);
-    //rpio.i2cWrite(new Buffer([MODE2, OUTDRV]));
-    //rpio.i2cWrite(new Buffer([MODE1, ALLCALL]));
-    //rpio.msleep(5); //wait or oscillator
-    //rpio.i2cRead()
-    //mode1 = self._device.readU8(MODE1)
-    //mode1 = mode1 & ~SLEEP //wake up (reset sleep)
-    //self._device.write8(MODE1, mode1)
-    rpio.msleep(5)  //wait for oscillator
+const swaggerOptions = {
+    info: {
+        title: 'Test API Documentation',
+        version: Pack.version,
+    },
 };
 
-var set_all_pwm = function set_all_pwm(on, off) {
-    //Sets all PWM channels.
-    write8(ALL_LED_ON_L, on & 0xFF)
-    write8(ALL_LED_ON_H, on >> 8)
-    write8(ALL_LED_OFF_L, off & 0xFF)
-    write8(ALL_LED_OFF_H, off >> 8)
-};
 
-var set_pwm = function set_pwm(channel, on, off) {
-    //Sets a single PWM channel.
-    write8(LED0_ON_L + 4 * channel, on & 0xFF)
-    write8(LED0_ON_H + 4 * channel, on >> 8)
-    write8(LED0_OFF_L + 4 * channel, off & 0xFF)
-    write8(LED0_OFF_H + 4 * channel, off >> 8)
-};
+server.route({
+    method: 'PUT',
+    path: '/setSpeed/{x}/{y}',
+    handler: handlers.setSpeed,
+    config: {
+        tags: ['api'],
+        validate: {
+            params: {
+                x: Joi.number().required(),
+                y: Joi.number().required()
+            },
+        }
+    }
+});
 
-rpio.i2cBegin();
-rpio.i2cSetSlaveAddress(PCA9685_ADDRESS);
-rpio.i2cSetBaudRate(100000);
-init();
-//for (var i = 0; i < 100; i++) {
-//    set_pwm(0, 0, 4094 - (40 * i));
-//    rpio.msleep(2000);
-//}
-rpio.msleep(2000);
-for(var i =0; i <= 12; i=i+2)
-{
-set_pwm(i,0,4093);
-//set_pwm(0,0,4093);
-set_pwm(i+1,0,0);
-rpio.msleep(2000);
-}
+server.route({
+    method: 'PUT',
+    path: '/stop',
+    handler: handlers.stop,
+    config: { tags: ['api'] }
+});
 
-rpio.i2cEnd();
+(async () => {
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+
+})();
+
